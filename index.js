626158422f90787f59ca1d8d675fa3a239b2a315
FFiillaattoovv <admirator2015@yandex.ru>
2022-01-18 13:56:17 +0300

    Adds functionality for saving files

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const hbs = require("hbs");
+const multer = require('multer');
 
 const app = express();
 
+const upload = multer({ dest: 'uploads/' });
+
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
+
+const storageConfig = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) =>{
+        cb(null, file.originalname);
+    }
+});
+
+app.use(express.static(__dirname));
+
+app.use(multer({storage:storageConfig}).single("fileData"));
 
 app.get('/', (req, res) => {
     res.render('index', {
@@ -18,6 +34,17 @@ app.get('/upload', (req, res) => {
         title: 'Upload',
         description: 'Upload the file'
     });
+});
+
+app.post('/upload', upload.single('fileData'), (req, res, next) => {
+    if (!req.file) {
+        res.send('Choose file');
+    } else {
+        res.render('upload', {
+            title: 'Upload',
+            description: 'Upload the file'
+        });
+    }
 });
 
 const PORT = 3000;
